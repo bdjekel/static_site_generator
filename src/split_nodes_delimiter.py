c73a0e570188e347b3c3b2c48934d5ea01ca31d5
bdjekel <bdjekel@gmail.com>
2024-10-10 16:25:31 -0500

    Added isolated delimiter test cases. Moving on from split_node_delimiter, but could return and add more tests if desired

--- src/split_nodes_delimiter.py
+++ src/split_nodes_delimiter.py
@@ -1,89 +1,102 @@
 from textnode import TextNode
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    # print(f' NNNNOOOOODDDDEEESSS {old_nodes}')
+
 # TODO Likely in a separate function, handle non-standard delimiters such as links, images, tables, etc.
 
     new_nodes = []
+
     for node in old_nodes:
         new_sub_nodes = []
         if node.text_type != "text":
-            new_sub_nodes.extend(node)
+            new_sub_nodes.extend([node])
         else:
             words = node.text.split()
             # print(words)
             delimiter_toggle = False
-            delimited_phrase = []
-            text_phrase = []
-            spacebar_transfer = False
-
+            delimited_word_list = []
+            text_word_list = []
+            spacebar_transfer = False # Used to preserve spaces adjacent to delimiters.
         for word in words:
-            # print(word)
+            # print(f'>>{word}<<')
         
-# Currently only evaluates delimiters at the start or end of words (all eight (8) scenarios described below). It does **NOT** catch delimiters _within_ words.
+# Currently only evaluates delimiters at the start or end of words (the ten (10) scenarios described below). It does **NOT** catch delimiters _within_ words.
+
+# Scenario (9): isolated delimiter, toggle off
+            if word == delimiter and not delimiter_toggle:
+                if len(text_word_list) > 0:
+                    text_string = ' '.join(text_word_list) + ' '
+                    new_sub_nodes.extend([TextNode(text_string, "text")])
+                    text_word_list.clear()
+                delimiter_toggle = True
+                spacebar_transfer = True
+
+# Scenario (10): isolated delimiter, toggle on
+            elif word == delimiter and delimiter_toggle:
+                delimited_string = ' '.join(delimited_word_list) + ' '
+                new_sub_nodes.extend([TextNode(delimited_string, text_type)])
+                delimited_word_list.clear()
+                delimiter_toggle = False
+                spacebar_transfer = True
 
 # Scenario (1): no delimiter, toggle off
-            if delimiter not in word and not delimiter_toggle:
+            elif delimiter not in word and not delimiter_toggle:
                 if spacebar_transfer:
                     spacebar_transfer = False
-                    text_phrase.extend([f' {word}'])
+                    text_word_list.extend([f' {word}'])
                 else:
-                    text_phrase.extend([word])
+                    text_word_list.extend([word])
 
 # Scenario (2): no delimiter, toggle on
             elif delimiter not in word and delimiter_toggle:
                 if spacebar_transfer:
                     spacebar_transfer = False
-                    delimited_phrase.extend([f' {word}'])
+                    delimited_word_list.extend([f' {word}'])
                 else:
-                    delimited_phrase.extend([word])
+                    delimited_word_list.extend([word])
 
 # Scenario (3): delimiter at start and end, toggle off
             elif word.startswith(delimiter) and word.endswith(delimiter) and not delimiter_toggle:
-            # Add any non-delimited text as a text node, then reset text_phrase
-                if len(text_phrase) > 0:
-                    text_string = ' '.join(text_phrase) + ' '
+                if len(text_word_list) > 0:
+                    text_string = ' '.join(text_word_list) + ' '
                     new_sub_nodes.extend([TextNode(text_string, "text")])
-                    text_phrase.clear()
-                    # # print('text_phrase cleared')
+                    text_word_list.clear()
                 delimiter_toggle = False
                 spacebar_transfer = True
                 new_sub_nodes.extend([TextNode(word.strip(delimiter), text_type)])
 
 # Scenario (4): delimiter at start and end, toggle on
             elif word.startswith(delimiter) and word.endswith(delimiter) and delimiter_toggle:
-                delimited_string = ' '.join(delimited_phrase) + ' '
+                delimited_string = ' '.join(delimited_word_list) + ' '
                 new_sub_nodes.extend([TextNode(delimited_string, text_type)])
-                delimited_phrase.clear()
+                delimited_word_list.clear()
                 new_sub_nodes.extend([TextNode(word.strip(delimiter), "text")])
                 delimiter_toggle = True
                 spacebar_transfer = True
 
 # Scenario (5): start delimiter, toggle off
             elif word.startswith(delimiter) and not delimiter_toggle:
-                # Add any non-delimited text as a text node, then reset text_phrase
-                if len(text_phrase) > 0:
-                    text_string = ' '.join(text_phrase) + ' '
+                if len(text_word_list) > 0:
+                    text_string = ' '.join(text_word_list) + ' '
                     new_sub_nodes.extend([TextNode(text_string, "text")])
-                    text_phrase.clear()
+                    text_word_list.clear()
                 delimiter_toggle = True
-                delimited_phrase.extend([word.strip(delimiter)])
+                delimited_word_list.extend([word.strip(delimiter)])
 
 # Scenario (6): start delimiter, toggle on
             elif word.startswith(delimiter) and delimiter_toggle:
-                text_phrase.extend([word.strip(delimiter)])
-                delimited_string = ' '.join(delimited_phrase) + ' '
+                text_word_list.extend([word.strip(delimiter)])
+                delimited_string = ' '.join(delimited_word_list) + ' '
                 new_sub_nodes.extend([TextNode(delimited_string, text_type)])
-            # Reset phrase list and toggle
-                delimited_phrase.clear()
+                delimited_word_list.clear()
                 delimiter_toggle = False
 
 # Scenario (7): end delimiter, toggle off
             elif word.endswith(delimiter) and not delimiter_toggle:
-                text_phrase.extend([word.strip(delimiter)])
-                text_string = ' '.join(text_phrase)
+                text_word_list.extend([word.strip(delimiter)])
+                text_string = ' '.join(text_word_list)
                 new_sub_nodes.extend([TextNode(text_string, "text")])
-                text_phrase.clear()
+                text_word_list.clear()
                 delimiter_toggle = True
                 spacebar_transfer = True
 
@@ -91,27 +104,25 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             elif word.endswith(delimiter) and delimiter_toggle:
                 if spacebar_transfer:
                     spacebar_transfer = False
-                    delimited_phrase.extend([f' {word.strip(delimiter)}'])
+                    delimited_word_list.extend([f' {word.strip(delimiter)}'])
                 else:
-                    delimited_phrase.extend([word.strip(delimiter)])
-                delimited_string = ' '.join(delimited_phrase)
+                    delimited_word_list.extend([word.strip(delimiter)])
+                delimited_string = ' '.join(delimited_word_list)
                 new_sub_nodes.extend([TextNode(delimited_string, text_type)])
-            # Reset phrase list and toggle
-                delimited_phrase.clear()
+                delimited_word_list.clear()
                 delimiter_toggle = False
                 spacebar_transfer = True
 
+            # print(f'text_word_list: {text_word_list}')
+            # print(f'delimited_word_list: {delimited_word_list}')
 
-            # print(f'text_phrase: {text_phrase}')
-            # print(f'delimited_phrase: {delimited_phrase}')
-
-# if delimited phrase is not ended by the end of the list, throw an error.
+# if delimited word_list is not ended by the end of the list, throw an error.
         if delimiter_toggle:
             raise Exception("Invalid Markdown syntax")
         
 # if words does not end with a delimiter, add text node for everything since the last delimiter 
-        if len(text_phrase) > 0:
-            text_string = ' '.join(text_phrase)
+        if len(text_word_list) > 0:
+            text_string = ' '.join(text_word_list)
             new_sub_nodes.extend([TextNode(text_string, "text")])
         
         # print(f'New Sub Nodes: {new_sub_nodes}')
--- src/test_split_nodes_delimiter.py
+++ src/test_split_nodes_delimiter.py
@@ -113,17 +113,93 @@ expected_nodes_4_strikethrough = [
     TextNode(" Kitchen Sink", "strikethrough")
     ]   
 
-old_italic_nodes = [node_1_italic, node_2_italic, node_3_italic, node_4_italic]
-old_bold_nodes = [node_1_bold, node_2_bold, node_3_bold, node_4_bold]
-old_strikethrough_nodes = [node_1_strikethrough, node_2_strikethrough, node_3_strikethrough, node_4_strikethrough]
+# node_5 can test these Scenarios:
+#     > (9) isolated delimiter, toggle off
+#     > (10) isolated delimiter, toggle on
+
+node_5_italic = TextNode('Texas: The Lone * Star * State', "text")
+node_5_bold = TextNode('The average distance between ** two stars ** is 5 light years', "text")
+node_5_strikethrough = TextNode('Three ~~ strikes ~~ and you\'reeeeee outta here', "text")
+
+# node_5 expected output:
+expected_nodes_5_italic = [
+    TextNode("Texas: The Lone ", "text"),
+    TextNode(" Star ", "italic"), 
+    TextNode(" State", "text")
+    ]
+expected_nodes_5_bold = [
+    TextNode("The average distance between ", "text"),
+    TextNode(" two stars ", "bold"), 
+    TextNode(" is 5 light years", "text")
+    ]
+expected_nodes_5_strikethrough = [
+    TextNode("Three ", "text"),
+    TextNode(" strikes ", "strikethrough"), 
+    TextNode(" and you'reeeeee outta here", "text")
+    ]  
+
+# node_6 can test these Scenarios:
+#     > (9) isolated delimiter, toggle off
+#     > (8) end delimiter, toggle on 
+
+
+node_6_italic = TextNode('Texas: The Lone * Star* State', "text")
+node_6_bold = TextNode('The average distance between ** two stars** is 5 light years', "text")
+node_6_strikethrough = TextNode('Three ~~ strikes~~ and you\'reeeeee outta here', "text")
+
+# node_6 expected output:
+expected_nodes_6_italic = [
+    TextNode("Texas: The Lone ", "text"),
+    TextNode(" Star", "italic"), 
+    TextNode(" State", "text")
+    ]
+expected_nodes_6_bold = [
+    TextNode("The average distance between ", "text"),
+    TextNode(" two stars", "bold"), 
+    TextNode(" is 5 light years", "text")
+    ]
+expected_nodes_6_strikethrough = [
+    TextNode("Three ", "text"),
+    TextNode(" strikes", "strikethrough"), 
+    TextNode(" and you'reeeeee outta here", "text")
+    ]  
+
+# node_7 can test these Scenarios:
+#     > (8) start delimiter, toggle off
+#     > (10) isolated delimiter, toggle on
+
+node_7_italic = TextNode('Texas: The Lone *Star * State', "text")
+node_7_bold = TextNode('The average distance between **two stars ** is 5 light years', "text")
+node_7_strikethrough = TextNode('Three ~~strikes ~~ and you\'reeeeee outta here', "text")
+
+# node_7 expected output:
+expected_nodes_7_italic = [
+    TextNode("Texas: The Lone ", "text"),
+    TextNode("Star ", "italic"), 
+    TextNode(" State", "text")
+    ]
+expected_nodes_7_bold = [
+    TextNode("The average distance between ", "text"),
+    TextNode("two stars ", "bold"), 
+    TextNode(" is 5 light years", "text")
+    ]
+expected_nodes_7_strikethrough = [
+    TextNode("Three ", "text"),
+    TextNode("strikes ", "strikethrough"), 
+    TextNode(" and you'reeeeee outta here", "text")
+    ]  
+
+old_italic_nodes = [node_1_italic, node_2_italic, node_3_italic, node_4_italic, node_5_italic, node_6_italic, node_7_italic]
+old_bold_nodes = [node_1_bold, node_2_bold, node_3_bold, node_4_bold, node_5_bold, node_6_bold, node_7_bold]
+old_strikethrough_nodes = [node_1_strikethrough, node_2_strikethrough, node_3_strikethrough, node_4_strikethrough, node_5_strikethrough, node_6_strikethrough, node_7_strikethrough]
 
 old_nodes_combined = old_italic_nodes + old_bold_nodes + old_strikethrough_nodes
 
-expected_italic_nodes = expected_nodes_1_italic + expected_nodes_2_italic + expected_nodes_3_italic + expected_nodes_4_italic
+expected_italic_nodes = expected_nodes_1_italic + expected_nodes_2_italic + expected_nodes_3_italic + expected_nodes_4_italic + expected_nodes_5_italic + expected_nodes_6_italic + expected_nodes_7_italic 
 
-expected_bold_nodes = expected_nodes_1_bold + expected_nodes_2_bold + expected_nodes_3_bold + expected_nodes_4_bold
+expected_bold_nodes = expected_nodes_1_bold + expected_nodes_2_bold + expected_nodes_3_bold + expected_nodes_4_bold + expected_nodes_5_bold + expected_nodes_6_bold + expected_nodes_7_bold
 
-expected_strikethrough_nodes = expected_nodes_1_strikethrough + expected_nodes_2_strikethrough + expected_nodes_3_strikethrough + expected_nodes_4_strikethrough
+expected_strikethrough_nodes = expected_nodes_1_strikethrough + expected_nodes_2_strikethrough + expected_nodes_3_strikethrough + expected_nodes_4_strikethrough + expected_nodes_5_strikethrough + expected_nodes_6_strikethrough + expected_nodes_7_strikethrough
 
 expected_nodes_combined = expected_italic_nodes + expected_bold_nodes + expected_strikethrough_nodes
 
@@ -141,18 +217,23 @@ class TestSplitNodesDelimiter(unittest.TestCase):
         self.assertEqual(resulting_strikethrough_nodes, expected_nodes_1_strikethrough)
 
     def test_all_scenarios_with_italic(self):
+        self.maxDiff = None
         resulting_italic_nodes = split_nodes_delimiter(old_italic_nodes, '*', "italic")
         self.assertEqual(resulting_italic_nodes, expected_italic_nodes)
 
     def test_all_scenarios_with_bold(self):
+        self.maxDiff = None
         resulting_bold_nodes = split_nodes_delimiter(old_bold_nodes, '**', "bold")
         self.assertEqual(resulting_bold_nodes, expected_bold_nodes)
 
     def test_all_scenarios_with_strikethrough(self):
+        self.maxDiff = None
         resulting_strikethrough_nodes = split_nodes_delimiter(old_strikethrough_nodes, '~~', "strikethrough")
         self.assertEqual(resulting_strikethrough_nodes, expected_strikethrough_nodes)
 
+
     # def test_all_scenarios_run_all_delimiters(self):
+    #     self.maxDiff = None
     #     nodes_with_italics_processed = split_nodes_delimiter(old_nodes_combined, '*', 'italic')
     #     nodes_with_bolds_processed = split_nodes_delimiter(nodes_with_italics_processed, '**', 'bold')
     #     nodes_with_strikethroughs_processed = split_nodes_delimiter(nodes_with_bolds_processed, '~~', 'strikethrough')
