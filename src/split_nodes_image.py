0034287a45d55530eeff21e7c77a1ca66b1fe92f
bdjekel <bdjekel@gmail.com>
2025-02-26 13:26:10 -0600

    small updates

--- src/split_nodes_image.py
+++ src/split_nodes_image.py
@@ -13,18 +13,10 @@ def split_nodes_image(old_nodes: list):
         for s in sub_nodes_text:
             if "![" in s:
                 image_data = extract_markdown_images(s)
-                print(f"IMAGE DATA ==> {image_data}")
                 sub_nodes.extend([TextNode(image_data[0][0], TextType.IMAGE, image_data[0][1])])
             else:
                 sub_nodes.extend([TextNode(s, TextType.TEXT)])
-        print(f"SUB_NODES ==> {sub_nodes}")
         new_nodes.extend(sub_nodes)
-
-
-
-            
-
-    print(f"NEW_NODES ==> {new_nodes}")
     return new_nodes
 
 
