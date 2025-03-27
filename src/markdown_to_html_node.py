a1ade5ff38e8a2c30fa9dd8b91221967ecb481a3
bdjekel <bdjekel@gmail.com>
2025-03-27 08:41:19 -0500

    added type hinting to markdown_to_html_node.py

--- src/markdown_to_html_node.py
+++ src/markdown_to_html_node.py
@@ -1,51 +1,56 @@
 from block_to_block_type import block_to_block_type, BlockType
+from htmlnode import HTMLNode
 from leafnode import LeafNode
 from markdown_to_blocks import markdown_to_blocks
 from parentnode import ParentNode
 from text_to_children import text_to_children
 
 #TODO: add type hinting to entire file
-#TODO: Add feature to allow for markdown within blockquotes
 
-def markdown_to_html_node(markdown):
-    wrapper_div = ParentNode("div", children=[])
-    markdown_blocks = markdown_to_blocks(markdown)
+def markdown_to_html_node(markdown: str) -> ParentNode:
+    wrapper_div: ParentNode = ParentNode("div", children=[])
+    markdown_blocks: list = markdown_to_blocks(markdown)
     
     for block in markdown_blocks:
-        block_type = block_to_block_type(block)
+        block_type: BlockType = block_to_block_type(block)
         match block_type:
+#TODO: Add feature to allow for markdown within headings?
             case BlockType.HEADING1 | BlockType.HEADING2 | BlockType.HEADING3 | BlockType.HEADING4 | BlockType.HEADING5 | BlockType.HEADING6:
-                block = block.strip("#")
-                block = block.strip()
-                heading_node = LeafNode(block_type.value, block)
+                block: str = block.strip("#")
+                block: str = block.strip()
+                heading_node: HTMLNode = LeafNode(block_type.value, block)
                 wrapper_div.children.append(heading_node)
             case BlockType.CODE:
-                block = block.strip("`")    # Since "`" is both a block level and inline level delimiter, must be stripped to avoid throwing error
-                code_node = LeafNode(block_type.value, block)
+                block: str = block.strip("`")
+                block: str = block.strip()
+                code_node: LeafNode = LeafNode(block_type.value, block)
                 wrapper_div.children.append(code_node)
+#TODO: Add feature to allow for markdown within blockquotes?
             case BlockType.QUOTE:
-                block = block.strip("> ")
-                quote_node = LeafNode(block_type.value, block)
+                block: str = block.strip("> ")
+                quote_node: HTMLNode = LeafNode(block_type.value, block)
                 wrapper_div.children.append(quote_node)
             case BlockType.UNORDERED_LIST:
-                ul_node = list_to_nodes(block, block_type.value)
+                ul_node: HTMLNode = list_to_nodes(block, block_type.value)
                 wrapper_div.children.append(ul_node)
             case BlockType.ORDERED_LIST:
-                ol_node = list_to_nodes(block, block_type.value)
+                ol_node: HTMLNode = list_to_nodes(block, block_type.value)
                 wrapper_div.children.append(ol_node)
             case _:
-                other_node = ParentNode(block_type.value, children=[])
-                children = text_to_children(block)
+                other_node: HTMLNode = ParentNode(block_type.value, children=[])
+                children: list[HTMLNode] = text_to_children(block)
                 other_node.children = children
                 wrapper_div.children.append(other_node)
     return wrapper_div
 
-def list_to_nodes(block, tag):
-    list_node = ParentNode(tag, children=[])
-    leaf_nodes = []
-    list_items = block.splitlines()
+def list_to_nodes(block: str, tag: str) -> HTMLNode | ValueError:
+    if block == "":
+        raise ValueError("Empty list not allowed.")    
+    list_node: ParentNode = ParentNode(tag, children=[])
+    leaf_nodes: list = []
+    list_items: list = block.splitlines()
     for item in list_items:
-        children = text_to_children(item)
+        children: list[LeafNode] = text_to_children(item)
         if len(children) == 0:
             leaf_nodes.append(LeafNode("li", item))
         else:
