5765c92d6fa657180f25b86cac31beced896dcec
bdjekel <bdjekel@gmail.com>
2025-03-29 07:13:51 -0500

    modified main.py to generate the static site. created generate_page.py to stage the converted markdown into an index.html file. created an index.md file to test with. created a template.html file to contain the html boilerplate. modified main.sh to start a local server.

---
+++ src/generate_page.py
@@ -0,0 +1,33 @@
+from extract_title import extract_title
+from htmlnode import HTMLNode
+from markdown_to_html_node import markdown_to_html_node
+import os
+
+def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+
+    with open(from_path, "r") as f:
+        markdown_content: str = f.read()
+
+    with open(template_path, "r") as f:
+        template_content: str = f.read()
+
+    node_tree: HTMLNode = markdown_to_html_node(markdown_content)
+
+    html_tree: str = node_tree.html()
+
+    title: str = extract_title(markdown_content)
+
+    template_with_title: str = template_content.replace("{{ Title }}", title)
+
+    template_completed: str = template_with_title.replace("{{ Content }}", html_tree)
+
+    dest_dir: str = os.path.dirname(dest_path)
+
+    if dest_dir:
+        os.makedirs(dest_dir, exist_ok=True)
+
+    with open(dest_path) as f:
+        f.write(template_completed)
+
+    
--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
+from generate_page import generate_page
 from move_content import move_content
+import os
+import shutil
 
 #TODO: incomplete
 
 def main() -> None:
+    
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+    os.mkdir("public")
+    
     move_content("static", "public")
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 main()
