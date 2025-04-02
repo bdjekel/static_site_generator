c1fd05dbdfa920ae1e88bad6eab13798057dc7af
bdjekel <bdjekel@gmail.com>
2025-04-01 21:24:08 -0500

    created build.sh to build site for Github Pages. Modified generate_page.py and generate_pages_recursive.py to include a basepath for the url. updated main.py to include basepath, and changed public filepaths to docs (for Github Pages).

--- src/generate_page.py
+++ src/generate_page.py
@@ -3,7 +3,7 @@ from htmlnode import HTMLNode
 from markdown_to_html_node import markdown_to_html_node
 import os
 
-def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
+def generate_page(from_path: str, template_path: str, dest_path: str, basepath: str) -> None:
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     with open(from_path, "r") as f:
@@ -19,8 +19,10 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
     title: str = extract_title(markdown_content)
 
 # TODO: Can I chain together .replaces and make this one line?
-    template_with_title: str = template_content.replace("{{ Title }}", title)
-    template_completed: str = template_with_title.replace("{{ Content }}", html_tree)
+    template_content: str = template_content.replace("{{ Title }}", title)
+    template_content = template_content.replace("{{ Content }}", html_tree)
+    template_content = template_content.replace('href="/', 'href="' + basepath)
+    template_content = template_content.replace('src="/', 'src="' + basepath)
 
     dest_dir: str = os.path.dirname(dest_path)
 
@@ -28,6 +30,6 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
         os.makedirs(dest_dir, exist_ok=True)
 
     with open(dest_path, "w") as f:
-        f.write(template_completed)
+        f.write(template_content)
 
     
--- src/main.py
+++ src/main.py
@@ -2,16 +2,24 @@ from generate_pages_recursive import generate_pages_recursive
 from move_content import move_content
 import os
 import shutil
+import sys
 
-#TODO: incomplete
+#TODO: turn your paths into variables at the top of the file.
 
 def main() -> None:
     
-    if os.path.exists("public"):
-        shutil.rmtree("public")
-    os.mkdir("public")
+    basepath: str = "/"
+    if len(sys.argv) > 1:
+        basepath: str = sys.argv[1]
+    print(basepath)
+
+    if os.path.exists("./docs"):
+        shutil.rmtree("./docs")
+    os.mkdir("./docs")
     
-    move_content("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    move_content("./static", "./docs")
+
+    
+    generate_pages_recursive("./content", "./template.html", "./docs", basepath)
 
 main()
